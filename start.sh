e1e78f0954e89b0be369df8c49b0834f2fabf888
Sander Dahlberg <sander.dahlberg@outlook.com>
2019-01-25 23:52:40 +0100

    enable remote debugging

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./wait-for-it.sh db:5432 -t 15
-java -Dspring.profiles.active=container -jar app.jar
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -Dspring.profiles.active=container -jar app.jar
